8587ece9af778b7cdcec2e6f3edce2b569a3235b
zsd <zhenshida@foxmail.com>
2022-07-25 22:05:52 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+import gym
+import json
+import datetime as dt
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines import PPO2
+
+from env.erp_gym_env import erp_gym_env
+
+import pandas as pd
+# The algorithms require a vectorized environment to run
+env = DummyVecEnv([lambda: erp_gym_env()])
+
+model = PPO2(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=20000)
+
+obs = env.reset()
+for i in range(2000):
+    action, _states = model.predict(obs)
+    obs, rewards, done, info = env.step(action)
+    env.render()
